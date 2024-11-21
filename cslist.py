bc03013a5b8df02d2b30d75a8ecba731fcda3edd
GilbertoB23 <111822725+GilbertoB23@users.noreply.github.com>
2024-11-21 00:18:48 -0800

    Fixed AGAIN cslist.py

--- cslist.py
+++ cslist.py
@@ -1,5 +1,5 @@
 cslist = [
-    ["Netflix", "Software Engineer Intern", "Los Angeles, CA", "https://explore.jobs.netflix.net/careers/job/790298739171?utm_source=Simplify&ref=Simplify", "09-04-2024"],
+    ["Netflix", "Software Engineer Intern", "Los Angeles, CA", "https://explore.jobs.netflix.net/careers/job/790298739171?utm_source=Simplify&ref=Simplify", "09-04-2024"], 
     ["TikTok", "Mobile Software Engineer Intern - User Relation", "Los Angeles, CA", "https://lifeattiktok.com/search/7390530196414613811?ref=Simplify&utm_source=Simplify", "07-19-2024"],
     ["Paramount", "Software Engineer Internships (Summer 2025 / Hybrid - NYC/LA)", "West Hollywood, CA", "https://careers.paramount.com/job/West-Hollywood-Software-Engineering-Internships-%28Summer-2025%2C-Hybrid-NYCLA%29-CA-90069/1229684800?utm_source=Simplify&ref=Simplify", "11-07-2024"],
     ["Blizzard Entertainment", "2025 US Summer Internship - AI Localization", "Irvine, CA", "https://activision.wd1.myworkdayjobs.com/Blizzard_External_Careers/job/Irvine---Blizzard---Blizzard-Way/Localization-Production-Internship_R024248?utm_source=Simplify&ref=Simplify", "11-04-2025"],
@@ -10,8 +10,8 @@ cslist = [
     ["Pacific Life", "Data Analytics Intern", "Newport Beach, CA", "https://pacificlife.wd1.myworkdayjobs.com/en-US/PacificLifeCareers/job/Newport-Beach-CA-700/Data-Analytics-Intern_R13239?utm_source=Simplify&ref=Simplify", "10-25-2024"],
     ["Pacific Life", "Data Engineering Intern", "Newport Beach, CA", "https://pacificlife.wd1.myworkdayjobs.com/en-US/PacificLifeCareers/job/Newport-Beach-CA-700/Data-Engineering-Intern_R13240?utm_source=Simplify&ref=Simplify", "10-25-2024"],
     ["Pacific Life", "Software Engineering Intern", "Newport Beach, CA", "https://pacificlife.wd1.myworkdayjobs.com/en-US/PacificLifeCareers/job/Newport-Beach-CA-700/Software-Engineering-Intern_R13241?utm_source=Simplify&ref=Simplify", "10-25-2024"],
-    ["March Industries", "Summer Intern 2025 - Software Engineer", "Huntington Beach, CA", "10-13-2024", "https://jobs.ashbyhq.com/mach/ae1694b8-509d-4ce3-84eb-0a4ccab4870e/application?utm_source=Simplify&ref=Simplify", "09-30-2024"],
+    ["March Industries", "Summer Intern 2025 - Software Engineer", "Huntington Beach, CA", "https://jobs.ashbyhq.com/mach/ae1694b8-509d-4ce3-84eb-0a4ccab4870e/application?utm_source=Simplify&ref=Simplify", "10-13-2024"],
     ["Google", "Software Engineer Intern", "Irvine, CA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"],
-    ["Google", "Software Engineer Intern", "Los Angeles, CA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify"],
+    ["Google", "Software Engineer Intern", "Los Angeles, CA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"],
     ["Trace3", "Data and Analytics Intern", "Irvine, CA", "https://boards.greenhouse.io/trace3/jobs/6375609?utm_source=Simplify&ref=Simplify", "11-05-2024"]
 ]
