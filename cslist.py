9338d6278eb48e026005d034e7ad952a6f3424ae
GilbertoB23 <111822725+GilbertoB23@users.noreply.github.com>
2024-12-09 17:11:49 -0800

    Added FAANG internships

--- cslist.py
+++ cslist.py
@@ -16,6 +16,34 @@ cslist = [
     ["Trace3", "Data and Analytics Intern", "Irvine, CA", "https://boards.greenhouse.io/trace3/jobs/6375609?utm_source=Simplify&ref=Simplify", "11-05-2024"]
 ]
 
+"""LIst of FAANG Internships in the US"""
+faangList = [
+  ["Amazon", "Software Development Engineer Internship in Test- 2025 (US)", "SunnyVale, CA", "not-available", "11-14-2024"]
+  ["Amazon", "Software Development Engineer Internship in Test- 2025 (US)", "Irvine, CA", "not-available", "11-14-2024"]
+  ["Amazon", "Software Development Engineer Internship in Test- 2025 (US)", "Seattle, Wa", "not-available", "11-14-2024"]
+  ["Amazon", "Software Development Engineer Internship - 2025 (US)", "Seattle, WA", "https://www.amazon.jobs/en/jobs/2808739/software-development-engineer-internship-2025-us?utm_source=Simplify&ref=Simplify", "10-17-2024"]
+  ["Amazon", "Software Dev Engineer Intern 2025 - Automated Reasoning", "Seattle, WA", "https://amazon.jobs/en/jobs/2798104/software-dev-engineer-intern-2025-automated-reasoning?utm_source=Simplify&ref=Simplify", "10-07-2024"]
+  ["Amazon", "Software Dev Engineer Intern 2025 - Automated Reasoning", "Arlington, VA", "https://amazon.jobs/en/jobs/2798104/software-dev-engineer-intern-2025-automated-reasoning?utm_source=Simplify&ref=Simplify", "10-07-2024"]
+  ["Amazon", "Software Dev Engineer Intern 2025 - Automated Reasoning", "Portland, OR", "https://amazon.jobs/en/jobs/2798104/software-dev-engineer-intern-2025-automated-reasoning?utm_source=Simplify&ref=Simplify", "10-07-2024"]
+  ["Amazon", "Software Dev Engineer Intern - Compiler - Annapurna ML", "Cupertino, CA", "https://amazon.jobs/en/jobs/2794068/2025-software-dev-engineer-intern-compiler-annapurna-ml?ref=Simplify&utm_source=Simplify", "10-03-2024"]
+  ["Amazon", "Software Dev Engineer Intern - Embedded - Runtime - Storage - System & Performance - Annapurna ML", "Cupertino, CA", "https://amazon.jobs/en/jobs/2794071/2025-software-dev-engineer-intern-embedded-runtime-storage-system-performance-annapurna-ml?ref=Simplify&utm_source=Simplify", "10-03-2024"]
+  ["Amazon", "Software Dev Engineer Intern - Embedded - Runtime - Storage - System & Performance - Annapurna ML", "Seattle, WA", "https://amazon.jobs/en/jobs/2794071/2025-software-dev-engineer-intern-embedded-runtime-storage-system-performance-annapurna-ml?ref=Simplify&utm_source=Simplify", "10-03-2024"]
+
+  ["Google", "Software Engineering Intern", "Atlanta, GA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+  ["Google", "Software Engineering Intern", "Austin, TX", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+  ["Google", "Software Engineering Intern", "Boulder, CO", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+  ["Google", "Software Engineering Intern", "Portlan, OR", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+  ["Google", "Software Engineering Intern", "Pittsburgh, PA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+  ["Google", "Software Engineering Intern", "San Francisco, CA", "https://www.google.com/about/careers/applications/jobs/results/133334514184135366-software-engineering-intern-bs-summer-2025?utm_source=Simplify&ref=Simplify", "09-30-2024"]
+
+  ["Netflix", "Software Engineer Intern", "Los Gatos, CA", "https://explore.jobs.netflix.net/careers/job/790298739171?utm_source=Simplify&ref=Simplify", "09-04-2024"]
+  ["Netflix", "Software Engineer Intern", "Los Angeles, CA", "https://explore.jobs.netflix.net/careers/job/790298739171?utm_source=Simplify&ref=Simplify", "09-04-2024"]
+
+  ["Netflix", "Data Visualization Engineer Intern", "Los Angeles, CA", "https://explore.jobs.netflix.net/careers/job/790299767067?utm_source=Simplify&ref=Simplify", "12-01-2024"]
+  ["Netlflix", "Data Visualization Engineer Intern", "Los Gatos, CA", "https://explore.jobs.netflix.net/careers/job/790299767067?utm_source=Simplify&ref=Simplify", "12-01-2024"]
+
+]
+
 """Copy and Paste this into their locaton"""
 
 from internshipsList import chemList, bioList, nurseList, econList, markList, financeList, psychList, crimList, polList, mathList, physicsList
